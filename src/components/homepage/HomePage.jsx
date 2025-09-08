f3963a3455a0ab5383b28823143c4ae88d36f80c
gamebos-collab <translovatogame@gmail.com>
2025-09-08 17:34:29 -0300

    atualizacao

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import * as XLSX from "xlsx";
-// ← Certifique-se de importar o rodapé
 import "./HomePage.css";
 
 export default function HomePage() {
