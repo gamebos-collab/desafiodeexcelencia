41b4a2ca718b3036785ca1eddd65929f36526555
gamebos-collab <translovatogame@gmail.com>
2025-09-08 15:46:20 -0300

    Renomeia temp.jsx para Ranking.jsx com capitalização correta

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EquipesPage from "./pages/EquipesPage";
-import Ranking from "./pages/Ranking";
-import Conquistas from "./pages/Conquistas";
+import Conquistas from "./pages/conquistas";
 import Regras from "./pages/Regras";
+import Ranking from "./pages/Ranking";
 
 export default function Router() {
   return (
