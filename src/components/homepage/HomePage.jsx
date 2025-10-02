13e0c7e2c7f54cbeec3c6a8b94a0ff8ba5343b21
gamebos-collab <translovatogame@gmail.com>
2025-10-02 08:41:48 -0300

    dia 2

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -93,7 +93,7 @@ export default function HomePage() {
           }}
         >
           <p style={{ color: "#ffc400ff" }}>
-            🕒 Última atualização do sistema: 01/10/2025 10:17h
+            🕒 Última atualização do sistema: 02/10/2025 08:42h
           </p>
         </motion.div>
 
