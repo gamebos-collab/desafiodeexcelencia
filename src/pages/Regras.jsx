95229ec59e3b647104fc5b53328af5e294a7c3b7
gamebos-collab <translovatogame@gmail.com>
2025-10-06 14:26:27 -0300

    regras

--- src/pages/Regras.jsx
+++ src/pages/Regras.jsx
@@ -111,14 +111,14 @@ function Regras() {
               <i>Finalizado (Parecer 6) ;</i>
 
               <li>
-                <i>5 a 10 dias em aberto →</i>
+                <i>6 a 10 dias em aberto →</i>
                 <span style={{ background: "#00ff0dff" }}>
                   {" "}
                   <b> 1 Ponto.</b>
                 </span>
               </li>
               <li>
-                <i>10 a 15 dias em aberto → </i>
+                <i>11 a 15 dias em aberto → </i>
                 <span style={{ background: "#00ff0dff" }}>
                   {" "}
                   <b> 2 Pontos.</b>
